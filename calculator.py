39bd5d7a468827ed5f647ec7f7ff2179f0738a25
jstevenson-tcspas <jstevenson-tcspas@outlook.com>
2025-01-11 16:29:04 -0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,16 @@
+# built-in function- it's comes installed with python, so you can use it without creating the function.
 
+# variable: a small container that stores small pieces of data (integers, strings, booleans, etc.)
+operator = input("Choose either +, -, *, or /:")
+while operator != "+" and operator != "-" and operator != "*" and operator != "/":
+    operator = input("Please choose again! ")
+
+
+
+# data casting- turning one data type into another temporarily
+
+
+ 
+first_number = input("choose a number:  ")
+
+second_number = input("chose another number to use:  ")
