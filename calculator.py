3a8e4943750b16f2f1cfc645ff2fca7d6a58cb6a
jstevenson-tcspas <jstevenson-tcspas@outlook.com>
2025-01-25 16:34:23 -0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,16 @@
 # built-in function- it's comes installed with python, so you can use it without creating the function.
 
 # variable: a small container that stores small pieces of data (integers, strings, booleans, etc.)
+
+# parameters: variables that you use in a function
+def add(bob, rob):
+    print(bob + rob)
+def sub(a, b):
+    print(a - b)
+def mult(c, d):
+    print(c * d)
+def div(e, f):
+    print(e / f)
 operator = input("Choose either +, -, *, or /:")
 while operator != "+" and operator != "-" and operator != "*" and operator != "/":
     operator = input("Please choose again! ")
@@ -10,7 +20,7 @@ while operator != "+" and operator != "-" and operator != "*" and operator != "/
 # data casting- turning one data type into another temporarily
 
 
-alphabet = "!@#$%^&*()_+=-qwertyuiopasdfghjklzxcvbnm,./;'[]"
+alphabet = "!@#$%^&*()_+=-qwertyuiopasdfghjklzxcvbnm,./;'[] "
 first_number = input("choose a number:  ")
 running = True
 while running:
@@ -31,3 +41,13 @@ while running:
         else:
             running = False
 
+# arguments: data you put in parameters
+
+if operator == "+":
+    add(int(first_number), int(second_number))
+elif operator == "-":
+    sub(int(first_number), int(second_number))
+elif operator == "*":
+    mult(int(first_number), int(second_number))
+elif operator == "/":
+    div(int(first_number), int(second_number))
